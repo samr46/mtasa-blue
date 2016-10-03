samr46/mtasa-blue

commit 293da30197f3f23da056b40906b3699d4f15d8a6

Fixed initial object freeze in new freezing method

Client/game_sa/CPhysicalSA.cpp
@@ -110,16 +110,8 @@ VOID CPhysicalSA::SetMoveSpeed(CVector * vecMoveSpeed)
 VOID CPhysicalSA::SetTurnSpeed(CVector * vecTurnSpeed)
 {
     DEBUG_TRACE("VOID CPhysicalSA::SetTurnSpeed(CVector * vecTurnSpeed)");
-    DWORD dwFunc = FUNC_GetTurnSpeed;
-    DWORD dwThis = (DWORD)((CPhysicalSAInterface *)this->GetInterface());
-    DWORD dwReturn = 0;
-    _asm
-    {
-        mov     ecx, dwThis
-        call    dwFunc
-        mov     dwReturn, eax
-    }
-    MemCpyFast ((void *)dwReturn, vecTurnSpeed, sizeof(CVector));
+    ((CPhysicalSAInterface *)this->GetInterface())->m_vecAngularVelocity = *vecTurnSpeed;
+    ((CPhysicalSAInterface *)this->GetInterface())->m_vecCollisionAngularVelocity = *vecTurnSpeed;
 }
 
 float CPhysicalSA::GetMass ( void )
@@ -374,6 +366,8 @@ void CPhysicalSA::SetLighting ( float fLighting )
 void CPhysicalSA::SetFrozen ( bool bFrozen )
 {
     CPhysicalSAInterface * pInterface = (CPhysicalSAInterface *)this->GetInterface();
+    // Set movement ability
     pInterface->bDisableMovement = bFrozen;
+    // Set rotation movement ability
     pInterface->bDisableFriction = bFrozen;
 }

Client/mods/deathmatch/logic/CClientObject.cpp
@@ -342,14 +342,30 @@ void CClientObject::Render ( void )
 void CClientObject::SetFrozen ( bool bFrozen )
 {
     m_bIsFrozen = bFrozen;
-    if ( m_pObject )
-        m_pObject->SetFrozen ( bFrozen );
 
+    if ( m_pObject )
+    {
+        m_pObject->SetFrozen ( bFrozen );
+    }
+
+    // Reset speed if we frozing object
     if ( bFrozen )
     {
+        // Reset speed only if object is actually moving
         CVector vecZero;
-        SetMoveSpeed ( vecZero );
-        SetTurnSpeed ( vecZero );
+        CVector vecSpeed;
+
+        GetMoveSpeed ( vecSpeed );
+        if ( vecZero != vecSpeed )
+        {
+            SetMoveSpeed ( vecZero );
+        }
+
+        GetTurnSpeed ( vecSpeed );
+        if ( vecZero != vecSpeed )
+        {
+            SetTurnSpeed ( vecZero );
+        }
     }
 }
 

commit 75009f6432a801f1f4a461e20894fbd9362c949b

Fix vehicle spinning issue
introduced in 293da30

Client/game_sa/CPhysicalSA.cpp
@@ -111,7 +111,6 @@ VOID CPhysicalSA::SetTurnSpeed(CVector * vecTurnSpeed)
 {
     DEBUG_TRACE("VOID CPhysicalSA::SetTurnSpeed(CVector * vecTurnSpeed)");
     ((CPhysicalSAInterface *)this->GetInterface())->m_vecAngularVelocity = *vecTurnSpeed;
-    ((CPhysicalSAInterface *)this->GetInterface())->m_vecCollisionAngularVelocity = *vecTurnSpeed;
 }
 
 float CPhysicalSA::GetMass ( void )