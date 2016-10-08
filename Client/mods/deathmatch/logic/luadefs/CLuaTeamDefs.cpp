samr46/mtasa-blue

commit da1709db93d060215067cad60533f621012cf2a2

Cleaned up some parts of client deathmatch
This commit mainly replaces iterator for loops with C++11 range based for loops for improved code readability

Client/mods/deathmatch/logic/luadefs/CLuaTeamDefs.cpp
@@ -161,10 +161,8 @@ int CLuaTeamDefs::GetPlayersInTeam ( lua_State* luaVM )
 
             unsigned int uiIndex = 0;
 
-            list < CClientPlayer* > ::const_iterator iter = pTeam->IterBegin ();
-            for ( ; iter != pTeam->IterEnd (); iter++ )
+            for ( auto& pPlayer : pTeam->GetChildList() )
             {
-                CClientPlayer* pPlayer = *iter;
                 if ( !pPlayer->IsBeingDeleted () )
                 {
                     lua_pushnumber ( luaVM, ++uiIndex );

commit 3e743ab8f3aa3f17cf7ab98858a052f0d0bb2b17

Fixed clientside getPlayersInTeam. Bug introduced in da1709db93d060215067cad60533f621012cf2a2

Client/mods/deathmatch/logic/luadefs/CLuaTeamDefs.cpp
@@ -161,7 +161,7 @@ int CLuaTeamDefs::GetPlayersInTeam ( lua_State* luaVM )
 
             unsigned int uiIndex = 0;
 
-            for ( auto& pPlayer : pTeam->GetChildList() )
+            for ( auto& pPlayer : pTeam->GetPlayers() )
             {
                 if ( !pPlayer->IsBeingDeleted () )
                 {